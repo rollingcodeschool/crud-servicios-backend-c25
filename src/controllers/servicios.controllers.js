c80709cd669a8805c9e5ba40229392fd6fc230bd
Emilse Arias <emilsearias@gmail.com>
2026-09-15 11:12:50 -0300

    agregamos el controlador para crear servicios

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -4,11 +4,14 @@ export const prueba = (req, res )=>{
     console.log('prueba de mi primer controlador')
     res.status(200).json({mensaje: 'Primer controlador exitoso ✅'})
 }
-export const crearServicio = (req, res )=>{
+export const crearServicio = async (req, res )=>{
     try {
-        console.log('aqui tengo que crear un servicio')
-        res.status(200).json({mensaje: 'aqui tengo que crear un servicio ✅'})
-        
+        console.log(req.body)
+        // deberia validar los datos del body
+        const servicioNuevo = new Servicio(req.body)
+        //guardar en la BD
+        await servicioNuevo.save()
+        res.status(201).json({mensaje: 'Servicio creado correctamente'})
     } catch (error) {
         console.error(error)
         res.status(500).json({mensaje: 'Ocurrio un error al intentar crear un servicio'})
