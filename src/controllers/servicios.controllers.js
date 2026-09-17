52a92841b840e5248394f3176b6adfe24a43d6eb
Emilse Arias <emilsearias@gmail.com>
2026-09-17 10:07:35 -0300

    agregamos el controlador y ruta para listar servicios

--- src/controllers/servicios.controllers.js
+++ src/controllers/servicios.controllers.js
@@ -1,19 +1,33 @@
-import Servicio from "../models/servicio.js"
+import Servicio from "../models/servicio.js";
 
-export const prueba = (req, res )=>{
-    console.log('prueba de mi primer controlador')
-    res.status(200).json({mensaje: 'Primer controlador exitoso ✅'})
-}
-export const crearServicio = async (req, res )=>{
-    try {
-        console.log(req.body)
-        // deberia validar los datos del body
-        const servicioNuevo = new Servicio(req.body)
-        //guardar en la BD
-        await servicioNuevo.save()
-        res.status(201).json({mensaje: 'Servicio creado correctamente'})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({mensaje: 'Ocurrio un error al intentar crear un servicio'})
-    }
-}
+export const prueba = (req, res) => {
+  console.log("prueba de mi primer controlador");
+  res.status(200).json({ mensaje: "Primer controlador exitoso ✅" });
+};
+export const crearServicio = async (req, res) => {
+  try {
+    console.log(req.body);
+    // deberia validar los datos del body
+    const servicioNuevo = new Servicio(req.body);
+    //guardar en la BD
+    await servicioNuevo.save();
+    res.status(201).json({ mensaje: "Servicio creado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar crear un servicio" });
+  }
+};
+
+export const listarServicios = async (req, res) => {
+  try {
+    const servicios = await Servicio.find();
+    res.status(200).json(servicios);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar listar los servicios" });
+  }
+};
--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { crearServicio, prueba } from "../controllers/servicios.controllers.js";
+import {
+  crearServicio,
+  listarServicios,
+  prueba,
+} from "../controllers/servicios.controllers.js";
 
-const router = Router()
+const router = Router();
 
 //http://localhost:3000/api/servicios/test
 //http://localhost:3000/api/servicios/
 // get, post, put/patch, delete
-router.route('/test').get(prueba)
-router.route('/').post(crearServicio)
+router.route("/test").get(prueba);
+router.route("/").post(crearServicio).get(listarServicios);
 
-export default router
+export default router;
